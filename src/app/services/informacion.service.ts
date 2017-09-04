9ad2b07a8ad64f3fc5113bf6173f905aa6d88735
Daniel Juarez <danieljuarezp@hotmail.com>
2017-09-03 19:46:17 -0600

    Se agrego servicio de firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,15 +5,29 @@ import { Http } from "@angular/http";
 export class InformacionService {
 
   info:any = {};
+  equipo:any[] =[];
   cargada:boolean = false;
+  cargada_sobre_nosotros:boolean = false;
 
   constructor( public http:Http ) { 
-    this.http.get("assets/data/info.pagina.json")
+   this.carga_info();
+   this.carga_sobre_nosotros();
+  }
+
+public carga_info(){
+   this.http.get("https://danieljuarez-c7e2b.firebaseio.com/equipo.json")
     .subscribe( data => {
-      console.log(data.json());
       this.cargada = true;
       this.info = data.json();
     })
+}
+
+  public carga_sobre_nosotros(){
+    this.http.get("https://danieljuarez-c7e2b.firebaseio.com/equipo.json")
+    .subscribe( data => {
+      this.cargada_sobre_nosotros = true;
+      this.equipo = data.json();
+    });
   }
 
 }
